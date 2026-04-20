c805ed82427435c19a274957fe20d980bad35377
ravimandavkar7 <140073182+ravimandavkar7@users.noreply.github.com>
2026-04-20 18:14:59 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -147,8 +147,11 @@ st.markdown("""
 # 3️⃣ Force Text Color
 st.markdown("""
 <style>
-h1, h2, h3, h4, h5, h6, p, div, span {
+h1, h2, h3, h4, h5, h6,  {
     color: white !important;
+}
+div, span{
+color: white 
 }
 </style>
 """, unsafe_allow_html=True)
