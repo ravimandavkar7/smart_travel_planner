55600d44c29a99a491ac3ce4752d1edd63ac27a5
ravimandavkar7 <140073182+ravimandavkar7@users.noreply.github.com>
2026-04-23 15:07:55 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -182,15 +182,10 @@ if search_clicked:
 st.title("Smart Travel Planner ✈️")
 
 if "shown_hint" not in st.session_state:
-    st.caption("📱 Tap >> on top-left for Find Best Destination")
+    st.caption("📱Using mobile? Tap >> on top-left for Find Best Destination")
     st.session_state.shown_hint = True
     
-    st.markdown("""
-    📱 **Using mobile?**
-
-    👉 Tap the **>> menu on top-left** to open filters and see recommended destinations.
-    """)
-
+    
 # Dropdown from DB
 cursor.execute("SELECT Destination FROM Destination")
 destinations = [row[0] for row in cursor.fetchall()]
