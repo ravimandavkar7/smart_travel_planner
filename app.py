317285e30ea99de3389cacab12eeb8e8c013d440
ravimandavkar7 <140073182+ravimandavkar7@users.noreply.github.com>
2026-04-29 15:53:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def month_in_season(user_month, season_range):
 if "show_ai_confirm" not in st.session_state:
     st.session_state.show_ai_confirm = False
 
-if "use_ai" not in st.session_state:
+if "use_ai" not in st.ses														sion_state:
     st.session_state.use_ai = False
 
 if "SUPABASE_URL" in st.secrets and "SUPABASE_KEY" in st.secrets:
@@ -487,6 +487,8 @@ if st.button("Generate Plan"):
     """, (destination_id,))
 
     places = cursor.fetchall()
+    
+    st.write(places)
     
     for place in places:
         st.markdown(f"""
